2430393cf186bb05026f5c33d76d9a267f218264
AlinJiong <alinjiong@qq.com>
2022-01-13 22:29:42 +0800

    一对多

--- demo1.py
+++ demo1.py
@@ -33,10 +33,34 @@ class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text, nullable=False)
+
+    # 外键
+    # 1、外键的数据类型与所引用的键的类型一致
+    # 2、db.ForeignKey(表名.字段)
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+    # relationship
+    # 1、第一个参数和模型绑定
+    # 2、backref(back reference): 反向引用，代表对方访问我的时候的字段名称
+    author = db.relationship('User', backref="articles")
+
+
+# 若新建关系，则先drop掉之前建立的表
+db.drop_all()
 
 db.create_all()
+
+
+@app.route('/otm')
+def one_to_many():
+    article1 = Article(title='1', content='1')
+    article2 = Article(title='2', content='2')
+    user = User(username='alin')
+    article1.author = user
+    article2.author = user
+    db.session.add(article1, article2)
+    db.session.commit()
+    return 'otm success！'
 
 
 @app.route("/article")
@@ -72,4 +96,4 @@ def test():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
